531155b22fc1ce8d4cb54d4832fb62daab5466f6
Ezequiel Nielsen Hillen <ezenh87@gmail.com>
2024-08-12 10:37:19 -0300

    vercel

--- server.js
+++ server.js
@@ -1,3 +1,165 @@
+// require('dotenv').config();
+
+// const express = require('express');
+// const mongoose = require('mongoose');
+// const bodyParser = require('body-parser');
+// const cors = require('cors');
+// const jwt = require('jsonwebtoken');
+
+
+// const app = express();
+// const port = process.env.PORT || 3000;
+
+// app.use(cors());
+// app.use(bodyParser.json());
+
+// // mongoose.connect('mongodb+srv://ezenh87:niZek8lQJ0CpQzSX@expount.ph9wkxg.mongodb.net/expount2024');
+// const MONGODB_URI = process.env.MONGODB_URI;
+// const SECRET_KEY = process.env.JWT_SECRET;
+
+// mongoose.connect(MONGODB_URI).
+//     then(() => {
+//     console.log('Conectado a MongoDB');
+// }).catch(err => {
+//     console.error('Error al conectar a MongoDB:', err.message);
+// });
+
+
+// const UserSchema = new mongoose.Schema({
+//     dni: { type: Number, unique: true, required: true },
+//     photo: String,
+//     name: { type: String, required: true },
+//     lastName: { type: String, required: true },
+//     gender: { type: String, required: true },
+//     birthDate: { type: String, required: true },
+//     email: { type: String, required: true, unique: true },
+//     school: { type: String, required: true },
+//     sorteo: { type: Boolean, default: false, required: true }
+// });
+
+// const User = mongoose.model('User', UserSchema);
+
+// app.get('/users', async (req, res) => {
+//     try {
+//         const users = await User.find(); // Encuentra todos los usuarios en la base de datos
+//         res.json(users); // Envía los usuarios como respuesta JSON
+//     } catch (error) {
+//         res.status(500).json({ error: error.message }); // Maneja errores
+//     }
+// });
+
+// app.post('/users', async (req, res) => {
+//     try {
+//         const userData = req.body;
+//         console.log('Datos recibidos:', userData);
+        
+//         // Verificar si ya existe un usuario con el mismo DNI
+//         const existingUserDNI = await User.findOne({ dni: userData.dni });
+//         if (existingUserDNI) {
+//             return res.status(400).json({ success: false, error: 'Ya existe un usuario con este DNI' });
+//         }
+        
+//         // Verificar si ya existe un usuario con el mismo email
+//         const existingUserEmail = await User.findOne({ email: userData.email });
+//         if (existingUserEmail) {
+//             return res.status(400).json({ success: false, error: 'Ya existe un usuario con este email' });
+//         }
+        
+//         const user = new User(userData);
+//         await user.save();
+//         const token = jwt.sign({ userId: user._id }, SECRET_KEY);
+//         res.json({ success: true, token, user: { user } });
+//     } catch (error) {
+//         console.error('Error al guardar el usuario:', error);
+//         res.status(400).json({ success: false, error: error.message });
+//     }
+// });
+
+// app.post('/check-dni', async (req, res) => {
+//     try {
+//         const { dni } = req.body;
+//         const user = await User.findOne({ dni });
+//         if (user) {
+//             const token = jwt.sign({ userId: user._id }, SECRET_KEY);
+//             res.json({ success: true, token, user: { user } });
+//         } else {
+//             res.json({ success: false, message: 'DNI no registrado' });
+//         }
+//     } catch (error) {
+//         res.status(400).json({ success: false, message: error.message });
+//     }
+// });
+
+// app.post('/login-google', async (req, res) => {
+//     try {
+//         const { email } = req.body;
+//         let user = await User.findOne({ email });
+//         if (!user) {
+//             // Si el usuario no existe, lo creamos
+//             user = new User(req.body);
+//             await user.save();
+//         }
+//         const token = jwt.sign({ userId: user._id }, SECRET_KEY);
+//         res.json({ success: true, token, user: { user } });
+//     } catch (error) {
+//         res.status(400).json({ success: false, error: error.message });
+//     }
+// });
+
+// app.get('/user/:id', authenticateToken, async (req, res) => {
+//     try {
+//         const user = await User.findById(req.params.id);
+//         if (user) {
+//             res.json({ success: true, user: { user } });
+//         } else {
+//             res.status(404).json({ success: false, error: 'Usuario no encontrado' });
+//         }
+//     } catch (error) {
+//         res.status(400).json({ success: false, error: error.message });
+//     }
+// });
+
+
+
+// app.post('/participar', authenticateToken, async (req, res) => {
+//     console.log('Solicitud recibida en /participar');
+
+//     try {
+//         const userId = req.body.userId; // Extrae userId desde el cuerpo de la solicitud
+//         const user = await User.findByIdAndUpdate(
+//             userId,
+//             { sorteo: true },
+//             { new: true }
+//         );
+
+//         if (!user) {
+//             return res.status(404).json({ error: 'Usuario no encontrado' });
+//         }
+
+//         res.json({ message: 'Participación registrada con éxito', user });
+//     } catch (error) {
+//         res.status(500).json({ error: error.message });
+//     }
+// });
+
+
+// function authenticateToken(req, res, next) {
+//     const authHeader = req.headers['authorization'];
+//     const token = authHeader && authHeader.split(' ')[1];
+//     if (token == null) return res.sendStatus(401);
+
+//     jwt.verify(token, SECRET_KEY, (err, user) => {
+//         if (err) return res.sendStatus(403);
+//         req.user = user;
+//         next();
+//     });
+// }
+
+// app.listen(port, () => {
+//     console.log(`Servidor corriendo en http://localhost:${port}`);
+// });
+
+
 require('dotenv').config();
 
 const express = require('express');
@@ -5,8 +167,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
-// const bcrypt = require('bcrypt');
-// const bcrypt = require('bcryptjs');
+const path = require('path'); // Nuevo: Para manejar rutas correctamente
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -14,18 +175,19 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(bodyParser.json());
 
-// mongoose.connect('mongodb+srv://ezenh87:niZek8lQJ0CpQzSX@expount.ph9wkxg.mongodb.net/expount2024');
+// Conexión a MongoDB
 const MONGODB_URI = process.env.MONGODB_URI;
 const SECRET_KEY = process.env.JWT_SECRET;
 
-mongoose.connect(MONGODB_URI).
-    then(() => {
-    console.log('Conectado a MongoDB');
-}).catch(err => {
-    console.error('Error al conectar a MongoDB:', err.message);
-});
+mongoose.connect(MONGODB_URI)
+    .then(() => {
+        console.log('Conectado a MongoDB');
+    })
+    .catch(err => {
+        console.error('Error al conectar a MongoDB:', err.message);
+    });
 
-
+// Schema y Modelos de Mongoose
 const UserSchema = new mongoose.Schema({
     dni: { type: Number, unique: true, required: true },
     photo: String,
@@ -40,6 +202,10 @@ const UserSchema = new mongoose.Schema({
 
 const User = mongoose.model('User', UserSchema);
 
+// Middleware para servir archivos estáticos desde la carpeta "public"
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Rutas de la API
 app.get('/users', async (req, res) => {
     try {
         const users = await User.find(); // Encuentra todos los usuarios en la base de datos
@@ -53,19 +219,19 @@ app.post('/users', async (req, res) => {
     try {
         const userData = req.body;
         console.log('Datos recibidos:', userData);
-        
+
         // Verificar si ya existe un usuario con el mismo DNI
         const existingUserDNI = await User.findOne({ dni: userData.dni });
         if (existingUserDNI) {
             return res.status(400).json({ success: false, error: 'Ya existe un usuario con este DNI' });
         }
-        
+
         // Verificar si ya existe un usuario con el mismo email
         const existingUserEmail = await User.findOne({ email: userData.email });
         if (existingUserEmail) {
             return res.status(400).json({ success: false, error: 'Ya existe un usuario con este email' });
         }
-        
+
         const user = new User(userData);
         await user.save();
         const token = jwt.sign({ userId: user._id }, SECRET_KEY);
@@ -120,8 +286,6 @@ app.get('/user/:id', authenticateToken, async (req, res) => {
     }
 });
 
-
-
 app.post('/participar', authenticateToken, async (req, res) => {
     console.log('Solicitud recibida en /participar');
 
@@ -143,7 +307,7 @@ app.post('/participar', authenticateToken, async (req, res) => {
     }
 });
 
-
+// Middleware para autenticación con JWT
 function authenticateToken(req, res, next) {
     const authHeader = req.headers['authorization'];
     const token = authHeader && authHeader.split(' ')[1];
@@ -156,7 +320,12 @@ function authenticateToken(req, res, next) {
     });
 }
 
+// Servir el archivo index.html para cualquier otra ruta
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Iniciar el servidor
 app.listen(port, () => {
     console.log(`Servidor corriendo en http://localhost:${port}`);
 });
-
